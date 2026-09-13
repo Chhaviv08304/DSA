084f128d62c28143c39b9bebb8e2455448607364
Chavi Verma <chhavixverma@gmail.com>
2026-09-13 15:59:46 +0530

    Time: 42 ms (91.67%), Space: 44.3 MB (85.23%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        int ans = 0;
+
+        // Try every possible row shift
+        for (int dr = -(n - 1); dr <= n - 1; dr++) {
+
+            // Try every possible column shift
+            for (int dc = -(n - 1); dc <= n - 1; dc++) {
+
+                int overlap = 0;
+
+                // Check every cell of img1
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+
+                        if (img1[i][j] == 1) {
+                            int ni = i + dr;
+                            int nj = j + dc;
+
+                            // Check if shifted position is inside matrix
+                            if (ni >= 0 && ni < n &&
+                                nj >= 0 && nj < n &&
+                                img2[ni][nj] == 1) {
+
+                                overlap++;
+                            }
+                        }
+                    }
+                }
+
+                ans = Math.max(ans, overlap);
+            }
+        }
+
+        return ans;
+    }
+}
